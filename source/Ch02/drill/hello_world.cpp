c500115367b62d5d0b8ddf0d6d6eeafb13879643
Kónya Márton <konyamarci02@protonmail.com>
2020-09-23 14:29:51 +0200

    drill ch02

---
+++ source/Ch02/drill/hello_world.cpp
@@ -0,0 +1,8 @@
+#include "std_lib_facilities.h"
+
+int main(){
+	cout << "Hello, World!\n";	//Output "Hello, World!"
+	keep_window_open();	// wait for a character to be entered
+	
+	return 0;
+}
---
+++ source/Ch03/drill/main.cpp
@@ -0,0 +1,12 @@
+#include "std_lib_facilities.h"
+
+int main(){
+	cout << "Enter the name of the person you want to write to (followed by 'enter'):\n";
+	string first_name;
+
+	cin >> first_name;
+
+	cout << "Dear, " << first_name << "!\n";
+	
+	cout << "How are you
+}
