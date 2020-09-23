d75e67802f16c66b1b9a7624fb748abfebb92fbe
Kónya Márton <konyamarci02@protonmail.com>
2020-09-23 14:51:55 +0200

    drill ch03

--- source/Ch03/drill/main.cpp
+++ source/Ch03/drill/main.cpp
@@ -8,5 +8,57 @@ int main(){
 
 	cout << "Dear, " << first_name << "!\n";
 	
-	cout << "How are you
+	cout << "How are you\nI am fine\nI miss you\n";
+
+	cout << "Enter the name of another friend: \n";
+
+	string friend_name;
+
+	cin >> friend_name;
+
+	cout << "Have you seen " << friend_name << " lately?\n";
+
+	char friend_sex = '0';
+	
+	cout << "Enter an 'm' if your friend is male or 'f' if female: \n";
+
+	cin >> friend_sex;
+
+	if(friend_sex == 'm'){
+		cout << "If you see " << friend_name << " please ask him to call me.\n";
+	}
+
+	if(friend_sex == 'f'){
+		cout << "If you see " << friend_name << " please ask her to call me.\n";
+	}
+
+	cout << "Enter the age of the recipient: \n";
+
+	int age;
+
+	cin >> age;
+
+	cout << "I hear you just had a birthday and you are " << age << " years old.\n";
+
+	if(age <= 0){
+		simple_error("you're kidding!");
+	}
+
+	if(age < 12){
+		cout << "Next year you will be " << age + 1 << ".\n";
+	}
+
+	if(age == 17){
+		cout << "Next year you will be able to vote." << endl;
+	}
+
+	if(age > 70){
+		cout << "I hope you are enjoying retirement." << endl;
+	}
+
+	cout << "Yours sincerely," << endl << endl;
+
+	cout << "Marci" << endl; 
+
+	return 0;
 }
